bf4a0206401f86c2021c4d027538fa2e3613ebaf
Erick Garcia <garcia.advincola@gmail.com>
2025-04-26 03:58:33 -0500

    Migraciones de clases

--- Restaurant/Datos/ApplicationDbContext.cs
+++ Restaurant/Datos/ApplicationDbContext.cs
@@ -13,5 +13,15 @@ namespace Restaurant.Datos
         protected ApplicationDbContext()
         {
         }
+
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<TipoPlato> TipoPlatos { get; set; }
+        public DbSet<Plato> Platos { get; set; }
+        public DbSet<TipoConsumo> TipoConsumos { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+        public DbSet<DetalleComanda> DetalleComandas { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+
     }
 }
---
+++ Restaurant/Migrations/20250426085613_Clases.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Migrations
+{
+    /// <inheritdoc />
+    public partial class Clases : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Direccion",
+                table: "AspNetUsers",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoConsumos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoConsumos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoPlatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoPlatos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Activo = table.Column<bool>(type: "bit", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mesas_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoPlatoId = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Platos_TipoPlatos_TipoPlatoId",
+                        column: x => x.TipoPlatoId,
+                        principalTable: "TipoPlatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comandas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MesaId = table.Column<int>(type: "int", nullable: true),
+                    UsuarioId = table.Column<int>(type: "int", nullable: true),
+                    UsuarioId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    TipoConsumoId = table.Column<int>(type: "int", nullable: true),
+                    EstadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comandas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comandas_AspNetUsers_UsuarioId1",
+                        column: x => x.UsuarioId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comandas_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comandas_Mesas_MesaId",
+                        column: x => x.MesaId,
+                        principalTable: "Mesas",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comandas_TipoConsumos_TipoConsumoId",
+                        column: x => x.TipoConsumoId,
+                        principalTable: "TipoConsumos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleComandas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComandaId = table.Column<int>(type: "int", nullable: false),
+                    PlatoId = table.Column<int>(type: "int", nullable: true),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Sugerencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleComandas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetalleComandas_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleComandas_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_DetalleComandas_Platos_PlatoId",
+                        column: x => x.PlatoId,
+                        principalTable: "Platos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ComandaId = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Facturas_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comandas_EstadoId",
+                table: "Comandas",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comandas_MesaId",
+                table: "Comandas",
+                column: "MesaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comandas_TipoConsumoId",
+                table: "Comandas",
+                column: "TipoConsumoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comandas_UsuarioId1",
+                table: "Comandas",
+                column: "UsuarioId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleComandas_ComandaId",
+                table: "DetalleComandas",
+                column: "ComandaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleComandas_EstadoId",
+                table: "DetalleComandas",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleComandas_PlatoId",
+                table: "DetalleComandas",
+                column: "PlatoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Facturas_ComandaId",
+                table: "Facturas",
+                column: "ComandaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesas_EstadoId",
+                table: "Mesas",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Platos_TipoPlatoId",
+                table: "Platos",
+                column: "TipoPlatoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleComandas");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "Platos");
+
+            migrationBuilder.DropTable(
+                name: "Comandas");
+
+            migrationBuilder.DropTable(
+                name: "TipoPlatos");
+
+            migrationBuilder.DropTable(
+                name: "Mesas");
+
+            migrationBuilder.DropTable(
+                name: "TipoConsumos");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Direccion",
+                table: "AspNetUsers",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
