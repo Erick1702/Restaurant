24ae5c6ccb0f09745564b47c9a6bea31a64f7085
Erick Garcia <garcia.advincola@gmail.com>
2025-06-15 23:42:30 -0500

    Implementacion speechSynthesis

--- Restaurant/Models/LoginViewModel.cs
+++ Restaurant/Models/LoginViewModel.cs
@@ -5,8 +5,10 @@ namespace Restaurant.Models
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "Correo Electrónico")]
         public required string Email { get; set; }
         [Required]
+        [Display(Name = "Contraseña")]
         public  required string Password { get; set; }
         public bool Recuerdame { get; set; }
     }
